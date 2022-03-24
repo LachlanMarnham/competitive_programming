896629cec87f3a8b5432a5ddaecd175221e090e9
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-24 19:11:22 +0000

    Use new solution

--- hackerrank/find_the_nearest_clone/solution.py
+++ hackerrank/find_the_nearest_clone/solution.py
@@ -1,8 +1,11 @@
 #!/bin/python3
 
-
+import math
+import os
+import random
+import re
+import sys
 from collections import defaultdict, deque
-
 # Complete the findShortest function below.
 
 #
@@ -13,48 +16,57 @@ from collections import defaultdict, deque
 # 3. An edge exists between <name>_from[i] to <name>_to[i].
 #
 #
-def bfs(node, nodes_with_colour, node_edge_map):
-    queue = deque([(node, 0)])
-    visited = set()
+def depth_first_search(start_node, graph, nodes_with_colour):
+    queue = deque([(start_node, 0)])
+    visited = {start_node}
     while queue:
-        current, count = queue.popleft()
-        visited.add(current)
-        for x in node_edge_map[current]:
-            if x not in visited:
-                if x in nodes_with_colour:
-                    return count + 1
-                else:
-                    visited.add(x)
-                    queue.append((x, count+1))
+        node, distance = queue.popleft()
+
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                if neighbour in nodes_with_colour:
+                    return distance + 1
+                visited.add(neighbour)
+                queue.append((neighbour, distance+1))
 
 
 def findShortest(graph_nodes, graph_from, graph_to, ids, val):
-    # Create the list of all nodes which have the target colour
-    nodes_with_colour = []
-    for idx, colour in enumerate(ids):
-        if colour == val:
-            nodes_with_colour.append(idx + 1)
+    nodes_with_colour = {idx + 1 for idx, n in enumerate(ids) if n == val}
 
-    # Create the representation of the graph: each key is a
-    # node, each value is a set of all nodes with which the key
-    # shares an edge
-    node_edge_map = defaultdict(set)
-    for i in range(len(graph_from)):
-        node_edge_map[graph_from[i]].add(graph_to[i])
-        node_edge_map[graph_to[i]].add(graph_from[i])
-
-    # Short-circuit: if there aren't at least 2 nodes
-    # with the target colour, we can't find a path
+    graph = defaultdict(set)
+    for edge in range(len(graph_from)):
+        vertex_from, vertex_to = graph_from[edge], graph_to[edge]
+        graph[vertex_from].add(vertex_to)
+        graph[vertex_to].add(vertex_from)
+    
     if len(nodes_with_colour) < 2:
         return -1
 
-    # Path length with have an item for each starting node,
-    # the value being the length of the shortest path to a
-    # node of the same colour
-    path_lengths = []
-
-    for node in nodes_with_colour:
-        path_length = bfs(node, nodes_with_colour, node_edge_map)
-        path_lengths.append(path_length)
+    path_lengths = set()
+    for start_node in nodes_with_colour:
+        path_length = depth_first_search(start_node, graph, nodes_with_colour)
+        path_lengths.add(path_length)
 
     return min(path_lengths)
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    graph_nodes, graph_edges = map(int, input().split())
+
+    graph_from = [0] * graph_edges
+    graph_to = [0] * graph_edges
+
+    for i in range(graph_edges):
+        graph_from[i], graph_to[i] = map(int, input().split())
+
+    ids = list(map(int, input().rstrip().split()))
+
+    val = int(input())
+
+    ans = findShortest(graph_nodes, graph_from, graph_to, ids, val)
+
+    fptr.write(str(ans) + '\n')
+
+    fptr.close()
