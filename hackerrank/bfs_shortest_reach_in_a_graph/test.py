6d15a01f159e3f06c28dead509197a588351ffed
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-28 11:35:10 +0100

    Fix test case names for bfs_shortest_reach_in_a_graph

--- hackerrank/bfs_shortest_reach_in_a_graph/test.py
+++ hackerrank/bfs_shortest_reach_in_a_graph/test.py
@@ -30,7 +30,7 @@ TEST_CASE_0 = """2
 2 3
 2"""
 
-TEST_CASE_1 = """1
+TEST_CASE_7 = """1
 6 4
 1 2
 2 3
@@ -38,7 +38,7 @@ TEST_CASE_1 = """1
 1 5
 1"""
 
-TEST_CASE_2 = """1
+TEST_CASE_8 = """1
 7 4
 1 2
 1 3
@@ -49,8 +49,8 @@ TEST_CASE_2 = """1
 
 @pytest.mark.parametrize('test_case, solution', [
     (TEST_CASE_0, ['6 6 -1', '-1 6']),
-    (TEST_CASE_1, ['6 12 18 6 -1']),
-    (TEST_CASE_2, ['6 12 18 6 -1 -1']),
+    (TEST_CASE_7, ['6 12 18 6 -1']),
+    (TEST_CASE_8, ['6 12 18 6 -1 -1']),
 ])
 def test(test_case, solution):
     assert hackerrank_main(test_case) == solution
