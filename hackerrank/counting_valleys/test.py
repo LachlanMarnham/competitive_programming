64d1cbdb6d91001daf03acde31b41c633af5c7a3
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-28 17:51:59 +0100

    Add test for counting_valleys

---
+++ hackerrank/counting_valleys/test.py
@@ -0,0 +1,25 @@
+import pytest
+
+from .solution import counting_valleys
+
+
+def hackerrank_main(test_case):
+    input_lines = test_case.splitlines()
+    steps = int(input_lines.pop(0).strip())
+    path = input_lines.pop(0)
+    return counting_valleys(steps, path)
+
+
+TEST_CASE_0 = """8
+UDDDUDUU"""
+
+TEST_CASE_1 = """12
+DDUUDDUDUUUD"""
+
+
+@pytest.mark.parametrize('test_case, solution', [
+    (TEST_CASE_0, 1),
+    (TEST_CASE_1, 2),
+])
+def test(test_case, solution):
+    assert hackerrank_main(test_case) == solution
