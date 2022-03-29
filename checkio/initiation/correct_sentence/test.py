49ecd5d46db95fcaa97c3a08e2272a805c23f759
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-29 21:55:13 +0100

    correct_sentence

---
+++ checkio/initiation/correct_sentence/test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from .solution import correct_sentence
+
+
+def checkio_main(text, solution):
+    assert correct_sentence(text) == solution
+
+
+@pytest.mark.parametrize('text, solution', [
+    ('greetings, friends', 'Greetings, friends.'),
+    ('Greetings, friends', 'Greetings, friends.'),
+    ('Greetings, friends.', 'Greetings, friends.'),
+    ('hi', 'Hi.'),
+    ('welcome to New York', 'Welcome to New York.'),
+])
+def test(text, solution):
+    checkio_main(text, solution)
