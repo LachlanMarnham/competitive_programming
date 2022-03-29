9f2caa0bb9879ad894d3fea7a0be47f1846914a1
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-29 15:42:49 +0100

    Add test for remove_all_before

---
+++ checkio/initiation/remove_all_before/test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from .solution import remove_all_before
+
+
+def checkio_main(items, border, solution):
+    assert remove_all_before(items, border) == solution
+
+
+@pytest.mark.parametrize('items, border, solution', [
+    ([1, 2, 3, 4, 5], 3, [3, 4, 5]),
+    ([1, 1, 2, 2, 3, 3], 2, [2, 2, 3, 3]),
+    ([1, 1, 2, 4, 2, 3, 4], 2, [2, 4, 2, 3, 4]),
+    ([1, 1, 5, 6, 7], 2, [1, 1, 5, 6, 7]),
+    ([], 0, []),
+    ([7, 7, 7, 7, 7, 7, 7, 7, 7], 7, [7, 7, 7, 7, 7, 7, 7, 7, 7]),
+])
+def test(items, border, solution):
+    checkio_main(items, border, solution)
