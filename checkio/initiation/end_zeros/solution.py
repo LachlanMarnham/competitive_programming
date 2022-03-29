358da5e0a05fb92935b20e54473324105f1ed51a
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-29 15:08:43 +0100

    Add solution for end_zeros

---
+++ checkio/initiation/end_zeros/solution.py
@@ -0,0 +1,9 @@
+def end_zeros(num: int) -> int:
+    num_str = str(num)
+    zeros_count = 0
+    for idx in range(len(num_str) - 1, -1, -1):
+        if num_str[idx] == '0':
+            zeros_count += 1
+        else:
+            break
+    return zeros_count
