b1e3f28141e758853fa96963b47d90c88f82b062
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-29 13:51:52 +0100

    Add test for acceptable_password_i

---
+++ checkio/initiation/acceptable_password_i/test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .solution import is_acceptable_password
+
+
+def checkio_main(password, solution):
+    assert is_acceptable_password(password) == solution
+
+
+@pytest.mark.parametrize('password, solution', [
+    ('short', False),
+    ('muchlonger', True),
+    ('ashort', False),
+])
+def test(password, solution):
+    checkio_main(password, solution)
