17047d58ad7098dbe9bd7dde387d7b3e4fee7d0c
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-03-29 14:12:28 +0100

    Add test for number_length

---
+++ checkio/initiation/number_length/test.py
@@ -0,0 +1,17 @@
+import pytest
+
+from .solution import number_length
+
+
+def checkio_main(a, solution):
+    assert number_length(a) == solution
+
+
+@pytest.mark.parametrize('a, solution', [
+    (10, 2),
+    (0, 1),
+    (4, 1),
+    (44, 2),
+])
+def test(a, solution):
+    checkio_main(a, solution)
