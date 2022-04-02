a34e6ea5f50b827c99f5680869a62f0adf75f7ba
Lachlan Marnham <lachlan.marnham@gmail.com>
2022-04-02 01:45:47 +0100

    majority

---
+++ checkio/oreilly/majority/test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from .solution import is_majority
+
+
+def checkio_main(items, solution):
+    assert is_majority(items) == solution
+
+
+@pytest.mark.parametrize('items, solution', [
+    ([True, True, False, True, False], True),
+    ([True, True, False], True),
+    ([True, True, False, False], False),
+    ([True, True, False, False, False], False),
+    ([False], False),
+    ([True], True),
+    ([], False),
+])
+def test(items, solution):
+    checkio_main(items, solution)
