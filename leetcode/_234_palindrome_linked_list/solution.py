fcda72509b2b48ee0a417e68c12279a99846bba4
LachlanMarnham <lachlan.marnham@gmail.com>
2023-01-18 21:04:09 +0000

    linked list palindrome

---
+++ leetcode/_234_palindrome_linked_list/solution.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def is_palindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        node = head
+        while node is not None:
+            stack.append(node.val)
+            node = node.next
+
+        node = head
+        while len(stack) > 0:
+            val = stack.pop()
+            if val != node.val:
+                return False
+            node = node.next
+        return True
