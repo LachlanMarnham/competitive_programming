3e7c6e17673880d79d637ea7582466ca5dd0f25f
LachlanMarnham <lachlan.marnham@gmail.com>
2023-01-23 22:41:01 +0000

    largest prime factor

--- project_euler/_3_largest_prime_factor/solution.py
+++ project_euler/_3_largest_prime_factor/solution.py
@@ -6,8 +6,8 @@ def is_factor(num: int, factor: int) -> bool:
 
 
 def is_prime(num):
-    for i in range(2, num):
-        if num % i == 0:
+    for divisor in range(2, num):
+        if num % divisor == 0:
             return False
     return True
 
