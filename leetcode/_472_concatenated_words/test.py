beb853559bcb0370053b35dcf47efcd9861ad6db
LachlanMarnham <lachlan.marnham@gmail.com>
2023-01-28 02:05:49 +0000

    black

--- leetcode/_472_concatenated_words/test.py
+++ leetcode/_472_concatenated_words/test.py
@@ -3,10 +3,16 @@ import pytest
 from .solution import Solution
 
 
-@pytest.mark.parametrize('words, output', (
-    (["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"], ["catsdogcats","dogcatsdog","ratcatdogcat"]),
-    (["cat","dog","catdog"], ["catdog"]),
-))
+@pytest.mark.parametrize(
+    "words, output",
+    (
+        (
+            ["cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"],
+            ["catsdogcats", "dogcatsdog", "ratcatdogcat"],
+        ),
+        (["cat", "dog", "catdog"], ["catdog"]),
+    ),
+)
 def test(words, output):
     tester = Solution()
     assert set(tester.find_concatenated_words(words)) == set(output)
