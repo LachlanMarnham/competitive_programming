5f1b96038b5513d6086a687ce3bb8a6059735f91
LachlanMarnham <lachlan.marnham@gmail.com>
2023-02-06 18:37:10 +0000

    Find all anagrams in a string

---
+++ leetcode/_438_find_all_anagrams_in_a_string/solution.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        len_p, len_s = len(p), len(s)
+        if len_p > len_s:
+            return []
+        p_counter = Counter(p)
+        s_counter = Counter(s[:len_p])
+        indexes = []
+        if p_counter == s_counter:
+            indexes.append(0)
+        for idx in range(len_s - len_p):
+            s_counter[s[idx]] -= 1
+            s_counter[s[len_p + idx]] += 1
+            if p_counter == s_counter:
+                indexes.append(idx + 1)
+        return indexes
---
+++ leetcode/_438_find_all_anagrams_in_a_string/test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from .solution import Solution
+
+
+@pytest.mark.parametrize(
+    "s, p, output",
+    (
+        ("cbaebabacd", "abc", [0, 6]),
+        ("abab", "ab", [0, 1, 2]),
+    ),
+)
+def test(s, p, output):
+    tester = Solution()
+    assert tester.findAnagrams(s, p) == output
